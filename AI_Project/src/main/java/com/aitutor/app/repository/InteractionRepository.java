28543e74547469d065aaf49c4b2710ee65d55f5c
Sharjeel <jalbanisharjeelkhan12@gmail.com>
2026-07-12 13:15:24 +0500

    Implemented Repository layer of backend
    Implemented Repository layer of backend, in which will communicate with ML Service

---
+++ AI_Project/src/main/java/com/aitutor/app/repository/InteractionRepository.java
@@ -0,0 +1,10 @@
+package com.aitutor.app.repository;
+
+import com.aitutor.app.entity.Interaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InteractionRepository extends JpaRepository<Interaction, Long> {
+    List<Interaction> findByStudentId(Long studentId);
+}
